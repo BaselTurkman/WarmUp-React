4f4a6889530c65e22de46a65b5f480d6fb4b3c03
BaselTurkman <Basel.Turkman123@gmail.com>
2025-04-06 02:00:16 +0300

    Remove Comment on membersList.js

--- src/constants/membersList.js
+++ src/constants/membersList.js
@@ -1,19 +1,9 @@
-// export const membersList = [
-//     "Carolien Bloeme",
-//     "Sun Jun",
-//     "Song Bao",
-//     "Olivia Arribas",
-//     "Bonginkosi Mdladlana",
-//     "Arina Belomestnykh",
-//     "Jacqueline Likoki",
-//   ];
-
 export const membersList = [
-  {name: "Carolien Bloeme", checked: false},
-  {name: "Sun Jun", checked: true},
-  {name: "Song Bao", checked: false},
-  {name: "Olivia Arribas", checked: true},
-  {name: "Bonginkosi Mdladlana", checked: false},
-  {name: "Arina Belomestnykh", checked: true},
-  {name: "Jacqueline Likoki", checked: true},
+  { name: "Carolien Bloeme", checked: false },
+  { name: "Sun Jun", checked: true },
+  { name: "Song Bao", checked: false },
+  { name: "Olivia Arribas", checked: true },
+  { name: "Bonginkosi Mdladlana", checked: false },
+  { name: "Arina Belomestnykh", checked: true },
+  { name: "Jacqueline Likoki", checked: true },
 ]
